9170609eb16373fa89fc06e84f07038af2a9fc9d
Dev-suite <kamaldeensulyman99@gmail.com>
2023-01-26 12:55:47 +0100

    Dark Mode

--- Weekend-project/Todo-List/app.js
+++ Weekend-project/Todo-List/app.js
@@ -21,8 +21,3 @@ document.querySelector('#push').onclick = function () {
         }
     }
 }
-
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
